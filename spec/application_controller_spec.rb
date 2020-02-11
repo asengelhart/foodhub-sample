4ec68b3e64e618f7383f046572429224ca43c00e
asengelhart <asengelhart@yahoo.com>
2020-02-10 17:59:50 -0700

    application spec

--- spec/application_controller_spec.rb
+++ spec/application_controller_spec.rb
@@ -12,9 +12,33 @@ describe ApplicationController do
 end
 
 describe ProducersController do
-  it 'displays a title' do
-    get '/producers'
-    expect(last_response.status).to eq(200)
-    expect(last_response.body).to include('Food Hub')
+  context "user is not logged in" do
+    describe 'get /producers' do
+      before do
+        get '/producers'
+        @response = last_response
+      end
+
+      it 'displays a title' do
+        expect(@response.status).to eq(200)
+        expect(@response.body).to include('Food Hub')
+      end
+
+      it 'has links for logging in and signing up' do
+        expect(@response.body).to include('Log In')
+        expect(@response.body).to include('Sign Up')
+      end
+    end
+
+    describe 'get /signup' do
+      it 'displays a signup form' do
+        visit '/signup'
+        expect(page).to have_selector('form')
+        expect(page).to have_field(:name)
+        expect(page).to have_field(:email)
+        expect(page).to have_field(:password)
+      end
+    end
   end
+  
 end
