49027e1286cbf321fbac1f9bac8278aa32f80907
asengelhart <asengelhart@yahoo.com>
2020-02-11 16:13:32 -0700

    rebuilt migrations

--- db/migrate/20200211002834_add_producer_id_to_items.rb
+++
@@ -1,5 +0,0 @@
-class AddProducerIdToItems < ActiveRecord::Migration
-  def change
-    add_column :items, :producer_id, :integer
-  end
-end
--- db/migrate/20200211231206_create_items.rb
+++ db/migrate/20200211231206_create_items.rb
@@ -1,9 +1,10 @@
-class CreateItems < ActiveRecord::Migration
+class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
       t.string :name
       t.integer :count
       t.integer :price_in_cents
+      t.integer :producer_id
     end
   end
 end
